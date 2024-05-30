5da47aca88bbb97e0ee929bfac8e7bc2d50722e2
achmadsholeh <achmaddsholehh@gmail.com>
2024-05-30 22:23:26 +0700

    Menu Vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Nav from "./components/Nav.vue";
+</script>
+
+<template>
+  <Nav />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <RouterView></RouterView>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+   <div class="card-body">Hello</div>
+  </div>
+
+  
+</template>
+
+<style scoped>
+
+</style>
